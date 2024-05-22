8decc92501a1fdf17c9358bef3fafbf7e4d0ea1c
Marcos Júnior <marcosajr11@gmail.com>
2024-05-21 23:19:39 -0300

    perf: better closing connections

--- src/main/java/com/portalaluno/dao/StudentDAO.java
+++ src/main/java/com/portalaluno/dao/StudentDAO.java
@@ -8,17 +8,19 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class StudentDAO {
-    private static final String INSERT_STUDENT_SQL = 
-            "INSERT INTO students (name, email, course )VALUES (?, ?, ?)";
-    
-    public void insertStudent(Student student) throws SQLException{
-        try (Connection conn = DB.getConnection();
-            PreparedStatement st = conn.prepareStatement(INSERT_STUDENT_SQL)){
-            st.setString(1, student.getName());
-            st.setString(2, student.getEmail());
-            st.setString(3, student.getCourse());
-            st.executeUpdate();
+
+    private static final String INSERT_STUDENT_SQL
+            = "INSERT INTO students (name, email, course )VALUES (?, ?, ?)";
+
+    public void insertStudent(Student student) throws SQLException {
+        try (Connection conn = DB.getConnection(); PreparedStatement st = conn.prepareStatement(INSERT_STUDENT_SQL)) {
+            if (conn != null) {
+                st.setString(1, student.getName());
+                st.setString(2, student.getEmail());
+                st.setString(3, student.getCourse());
+                st.executeUpdate();
+            }
         } catch (SQLException e) {
-        } 
+        }
     }
 }
